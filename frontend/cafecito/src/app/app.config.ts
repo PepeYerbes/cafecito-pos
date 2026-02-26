510d1520a11bcfdf395186a42835f41679a921e8
PepeYerbes <ppyerbes@gmail.com>
2026-02-26 00:22:03 -0600

    fix: provideAnimations en lugar de provideAnimationsAsync

--- frontend/cafecito/src/app/app.config.ts
+++ frontend/cafecito/src/app/app.config.ts
@@ -2,13 +2,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { APP_ROUTES } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimationsAsync(),
+    provideAnimations(),
     provideHttpClient(
       withFetch(),
       withInterceptors([authInterceptor])
