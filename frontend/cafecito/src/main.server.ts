a55e157b97d1e3fa8bd32e9a4d8dc6e0393a3de6
PepeYerbes <ppyerbes@gmail.com>
2026-02-26 02:17:52 -0600

    fix: remove all SSR files for Render static deployment

--- frontend/cafecito/src/main.server.ts
+++
@@ -1,9 +0,0 @@
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
-import { config } from './app/app.config.server';
-import 'zone.js';   
-
-const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(AppComponent, config, context);
-
-export default bootstrap;
